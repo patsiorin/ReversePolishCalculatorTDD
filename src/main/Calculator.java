9b0465535f77d18323853ef8c76b2383669be786
web <dekrement@gmail.com>
2018-03-15 11:59:10 +0300

    add implementation for entering and dropping values

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,22 +1,31 @@
 import java.math.BigDecimal;
+import java.util.Stack;
 
 /**
  * Created by Valerii Patsiorin on 15.03.2018.
  */
 public class Calculator {
+    private Stack<BigDecimal> values = new Stack();
     private BigDecimal accumulator = BigDecimal.ZERO;
 
     public BigDecimal getAccumulator() {
-        return accumulator;
+        if (values.size() == 0)
+            return BigDecimal.ZERO;
+        return values.peek();
     }
 
     public void setAccumulator(BigDecimal accumulator) {
-        this.accumulator = accumulator;
+        if (values.size() > 0)
+            values.pop();
+        values.push(accumulator);
     }
 
     public void enter() {
+        values.push(getAccumulator());
+
     }
 
     public void drop() {
+        values.pop();
     }
 }
